8505432ab88bad19c5c91c0965e9abf04bdf6a81
HeadMad <kov.ps@ya.ru>
2021-08-18 02:36:49 +0800

    Add sortable action

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,53 @@
 <script>
   // Supabase passwors DAJ-9xx-mPS-4hf
   import { onMount } from "svelte";
-  import Input from './components/Input/Input.svelte';
-  import InputList from './components/Input/InputList.svelte';
+  import { sortable } from "./components/DragDrop/actions";
 
   import AddRowForm from "./bundles/AddRowForm.svelte";
   let addForm;
-  // let value;
-  let items = ["Hello", "Dolly", "How", 'Hello'];
-  let value='some';
-  onMount(() => {});
-
+  let draggable = true;
 </script>
 
-<AddRowForm bind:this={addForm} />
-<button on:click={addForm.show}>Show</button>
-<!-- <Input {value} label="Awesome Input">
-  {#if items.length}
-  <InputList
-    keyActions={{Escape({list}) {list.toggle()}}}
-    list={items}
-    on:enter={ (e) => { items = items.concat(e.detail.value) }}
-    let:item
-  >Пункт {item} </InputList>
-  {/if}
-</Input> -->
+<!-- <AddRowForm bind:this={addForm} /> -->
+<!-- <button on:click={addForm.show}>Show</button> -->
+
+<ul use:sortable={draggable}>
+  <li>One</li>
+  <li>Two</li>
+  <li>Three</li>
+  <li>Four</li>
+  <li>Five</li>
+</ul>
+
+
+
+<button
+  on:click={() => draggable = !draggable}
+>{draggable ? 'Stop' : 'Start'}</button>
 
 <style>
+  :global(body) {
+    background-color: darksalmon;
+  }
+
+  ul {
+    display: inline-block;
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    border-radius: 5px;
+    padding: 12px;
+    min-width: 100px;
+  }
+
+  li {
+    list-style: none;
+    background-color: #fafafa;
+    padding: 5px 12px;
+    border-radius: 3px;
+    text-align: center;
+    cursor: grab;
+  }
+
+  li + li {
+    margin-top: 10px;
+  }
 </style>
